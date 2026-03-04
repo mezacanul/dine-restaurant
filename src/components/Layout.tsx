ee319f6993529b6bca8a10f2401533099e05430b
Eduardo Meza <joseduardomezacanul@hotmail.com>
2026-03-03 23:49:44 -0600

    Feat: Added layout, header, footer default and Homepage/Hero

---
+++ src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import { Outlet } from "react-router-dom";
+
+export default function Layout() {
+    return (
+        <div className="relative">
+            <Header />
+            <Outlet />
+            <Footer />
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <div className="absolute top-0 left-0 w-full px-container py-[3rem]">
+            <h1 className="text-white text-5xl font-bold">
+                {"dine"}
+            </h1>
+        </div>
+    );
+}
+
+function Footer() {
+    return (
+        <div className="w-full px-container">
+            <h1>{"Footer"}</h1>
+        </div>
+    );
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,36 @@
+import { cn } from "../utils/cn";
+
 export default function Home() {
+    const cns = {
+        container: cn(
+            "px-container",
+            "bg-[url('/images/homepage/hero-bg-desktop@2x.jpg')] bg-cover bg-center",
+            "h-screen w-full flex items-center text-white"
+        ),
+        content: cn(
+            "w-[500px] flex flex-col gap-[1.5rem] items-start"
+        ),
+        title: cn("text-1"),
+        description: cn("text-4-regular"),
+        button: cn(
+            "text-4-regular py-[1rem] px-[3rem] border border-white"
+        ),
+    };
     return (
-        <div>
-            <h1>Hello Home!</h1>
+        <div className={cns.container}>
+            <div className={cns.content}>
+                <h1 className={cns.title}>
+                    {"Exquisite dining since 1989"}
+                </h1>
+                <p className={cns.description}>
+                    {
+                        "Experience our seasonal menu in beautiful country surroundings. Eat the freshest produce from the comfort of our farmhouse."
+                    }
+                </p>
+                <button className={cns.button}>
+                    {"BOOK A TABLE"}
+                </button>
+            </div>
         </div>
     );
 }
